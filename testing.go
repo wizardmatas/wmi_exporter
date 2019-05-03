0a380001ef468e7843a10e4049c30709c93b4e11
Matas Tvarijonas <matas.tvarijonas@adform.com>
2019-05-03 14:57:18 +0300

    fix

--- testing.go
+++ testing.go
@@ -1,29 +1,30 @@
 package main
-import (
-//	"strings"
-    "fmt"
-//	"github.com/StackExchange/wmi"
-    "golang.org/x/sys/windows/registry"
-	"github.com/prometheus/common/log"
-	"strconv"
-)
 
+import (
+	//	"strings"
+	"fmt"
+	"strings"
+	//	"github.com/StackExchange/wmi"
+	"strconv"
+
+	"github.com/prometheus/common/log"
+	"golang.org/x/sys/windows/registry"
+)
 
 func main() {
 	version := getWindowsVersion()
 	wmitesting := wmitest()
-    fmt.Println("hello world")
+	fmt.Println("hello world")
 	fmt.Println(version)
 	if version > 8 {
-	    fmt.Println("Dagiau")
+		fmt.Println("Dagiau")
 	} else {
 		fmt.Println("Maziau")
 	}
-	
-	
+
 }
 
-func getWindowsVersion()(float64) {
+func getWindowsVersion() float64 {
 	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Microsoft\Windows NT\CurrentVersion`, registry.QUERY_VALUE)
 	if err != nil {
 		log.Warn("Couldn't open registry", err)
@@ -36,12 +37,10 @@ func getWindowsVersion()(float64) {
 	}
 
 	log.Info("Detected Windows %d.%d\n", major)
-	major_flt, err := strconv.ParseFloat(major,64)
+	major_flt, err := strconv.ParseFloat(major, 64)
 	log.Info(major_flt)
 	return major_flt
-	}
-	
-
+}
 
 func wmitest() {
 	var dst []Win32_PerfRawData_Counters_ProcessorInformation
@@ -55,9 +54,5 @@ func wmitest() {
 		if strings.Contains(data.Name, "_Total") {
 			continue
 		}
-
-
-
-
-
-
+	}
+}
